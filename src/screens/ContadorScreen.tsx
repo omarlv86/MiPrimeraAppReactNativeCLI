d3c0a405c7e2b95fd8a0a2a02c402e2fb9aeed02
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-08-27 17:38:21 -0500

    Main | Touchable Opacity

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Text, View } from 'react-native'
+import { Button, Text, TouchableOpacity, View } from 'react-native'
 
 export const ContadorScreen = () => {
 
@@ -18,10 +18,16 @@ export const ContadorScreen = () => {
                 Contador: { contador }
             </Text>
 
-            <Button
-              title="Click"
-              onPress={() => setContador( contador + 1 )}
-            />
+            <TouchableOpacity
+              onPress={ () => setContador(contador + 1) }
+            >
+                <View style={{
+                    backgroundColor:'#a2a2a2',
+                    borderRadius: 100
+                }}>
+                  <Text>Click me!!</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     )
 }
