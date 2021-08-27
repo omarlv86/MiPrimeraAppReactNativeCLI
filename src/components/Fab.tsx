c0cd6e298cef9b901dd56d60ce2f14fb0353ebdb
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-08-27 18:51:20 -0500

    Main | codigo especifico de la plataforma

--- src/components/Fab.tsx
+++ src/components/Fab.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native'
+import { StyleSheet, Text, TouchableNativeFeedback, View, Platform, TouchableOpacity } from 'react-native'
 
 interface Props {
     title: string;
@@ -7,23 +7,45 @@ interface Props {
     onPress: () => void;
 }
 export const Fab = ({title, onPress, position = 'br'}: Props) => {
-    console.log(title)
-    return (
-        <View style={[
-            styles.fabLocation, 
-            ( position === 'bl') ? styles.left : styles.right
-        ]}>
-            <TouchableNativeFeedback
-              onPress={ onPress }
-              background={ TouchableNativeFeedback.Ripple('#28425B', false, 30)}
-            >
+    const ios = () => {
+        return (
+            <TouchableOpacity 
+              activeOpacity={ 0.5 }
+              style={[
+                styles.fabLocation, 
+                ( position === 'bl') ? styles.left : styles.right
+            ]}>
+                
                 <View style={styles.fab}>
-                  <Text style={ styles.fabText}>{ title }</Text>
+                    <Text style={ styles.fabText}>{ title }</Text>
                 </View>
-            </TouchableNativeFeedback>
-        </View>
-        
-    )
+            </TouchableOpacity>
+            
+        )
+    }
+
+    const android = () => {
+        return (
+            <View style={[
+                styles.fabLocation, 
+                ( position === 'bl') ? styles.left : styles.right
+            ]}>
+                <TouchableNativeFeedback
+                  onPress={ onPress }
+                  background={ TouchableNativeFeedback.Ripple('#28425B', false, 30)}
+                >
+                    <View style={styles.fab}>
+                      <Text style={ styles.fabText}>{ title }</Text>
+                    </View>
+                </TouchableNativeFeedback>
+            </View>
+            
+        )
+    }
+
+    //console.log(title)
+    return (Platform.OS === 'ios') ? ios() : android()
+    
 }
 
 const styles = StyleSheet.create({
